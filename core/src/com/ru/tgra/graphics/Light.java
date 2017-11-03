eee213fc48450629a6e2fb864885c208063f7406
Hlynur Stefánsson <hlynurs15@ru.is>
2017-11-03 22:56:27 +0000

    Add Light class

---
+++ core/src/com/ru/tgra/graphics/Light.java
@@ -0,0 +1,92 @@
+package com.ru.tgra.graphics;
+
+import com.ru.tgra.utils.Color;
+import com.ru.tgra.utils.Point3D;
+import com.ru.tgra.utils.Vector3D;
+
+public class Light {
+    private int index;
+    private Point3D position;
+    private Vector3D direction;
+    private Color color;
+
+    private float spotExponent;
+    private float constantAttenuation;
+    private float linearAttenuation;
+    private float quadraticAttenuation;
+
+    public Light(int id, Point3D position, Vector3D direction, Color color, float spotExponent, float constantAttenuation, float linearAttenuation, float quadraticAttenuation) {
+        this.index = id;
+        this.position = position;
+        this.direction = direction;
+        this.color = color;
+        this.spotExponent = spotExponent;
+        this.constantAttenuation = constantAttenuation;
+        this.linearAttenuation = linearAttenuation;
+        this.quadraticAttenuation = quadraticAttenuation;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Point3D getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point3D position) {
+        this.position = position;
+    }
+
+    public Vector3D getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector3D direction) {
+        this.direction = direction;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public float getSpotExponent() {
+        return spotExponent;
+    }
+
+    public void setSpotExponent(float spotExponent) {
+        this.spotExponent = spotExponent;
+    }
+
+    public float getConstantAttenuation() {
+        return constantAttenuation;
+    }
+
+    public void setConstantAttenuation(float constantAttenuation) {
+        this.constantAttenuation = constantAttenuation;
+    }
+
+    public float getLinearAttenuation() {
+        return linearAttenuation;
+    }
+
+    public void setLinearAttenuation(float linearAttenuation) {
+        this.linearAttenuation = linearAttenuation;
+    }
+
+    public float getQuadraticAttenuation() {
+        return quadraticAttenuation;
+    }
+
+    public void setQuadraticAttenuation(float quadraticAttenuation) {
+        this.quadraticAttenuation = quadraticAttenuation;
+    }
+}
